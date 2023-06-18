7ea59e369f17af456c99ddec61692f3a25f62fd4
nxobilebhila <nxobilebhilab@gmail.com>
2023-06-18 09:38:15 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry
  *
- * Return: Always (0)
+ * Return: Always(0)
  */
 int main(void)
 { 
 	printf("which proper grammer, but the outcome is a piece of art,\n");
-	Return (0); 
+	Return(0); 
 }
