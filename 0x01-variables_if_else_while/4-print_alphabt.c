2c09c6c70788f2e8b531e391809bb44bd10b8c1d
nxobilebhila <nxobilebhilab@gmail.com>
2023-06-18 13:28:12 -0700

    print alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	char ch = 'a';
 while (ch <= 'z')
 {
-if (ch != 'e' && ch != 'q')
+	if (ch != 'e' && ch != 'q')
 {
-putchar(ch);
+	putchar(ch);
 }
-ch++; 
+ch++;
 }
 putchar('\n');
 return (0);
